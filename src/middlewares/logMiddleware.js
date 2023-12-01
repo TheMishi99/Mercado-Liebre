07b009647ae03993c364c1d4be47ec2568ecafaf
Matias Demian Sayago <matiasgd99@gmail.com>
2023-12-01 19:41:51 -0300

    VIEWS-STYLES ADDED

--- src/middlewares/logMiddleware.js
+++ src/middlewares/logMiddleware.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const logPath = path.join(__dirname, './logs/app.log');
+const logPath = path.join(__dirname, 'main-log.txt');
 
 const logMiddleware = (req, res, next) => {
     let info = "Se ingresó a: " + req.url + "\n";
